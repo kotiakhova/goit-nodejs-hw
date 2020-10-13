9dfae9d2c7794dd5c4efd6ee5f695de1b27476f4
kotiakhova <kotiakhovaa@gmail.com>
2020-10-13 16:40:19 +0300

    hw-2

--- contacts.js
+++ contacts.js
@@ -4,7 +4,6 @@ const { promises: fsPromises } = fs;
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
-// TODO: задокументировать каждую функцию
 function listContacts() {
   fs.readFile(contactsPath, "utf-8", (err, data) => {
     console.table(JSON.parse(data));
@@ -13,9 +12,7 @@ function listContacts() {
 
 function getContactById(contactId) {
   fs.readFile(contactsPath, "utf-8", (err, data) => {
-    const contact = JSON.parse(data).find(
-      (contact) => contact.id === contactId
-    );
+    const contact = JSON.parse(data).find(contact => contact.id === contactId);
     console.log(contact);
   });
 }
@@ -23,9 +20,9 @@ function getContactById(contactId) {
 function removeContact(contactId) {
   fs.readFile(contactsPath, "utf-8", (err, data) => {
     const newcContacts = JSON.parse(data).filter(
-      (contact) => contact.id != contactId
+      contact => contact.id != contactId
     );
-    fs.writeFile(contactsPath, JSON.stringify(newcContacts), (err) => {
+    fs.writeFile(contactsPath, JSON.stringify(newcContacts), err => {
       if (err) console.log(err);
     });
   });
@@ -54,35 +51,10 @@ async function addContact(name, email, phone) {
     throw err;
   }
 }
-// async function addContact(name, email, phone) {
-//   try {
-//     const contactsList = JSON.parse(
-//       await fsPromises.readFile("./db/contacts.json", "utf-8")
-//     );
-//     const newContact = {
-//       id: contactsList.length + 1,
-//       name,
-//       email,
-//       phone,
-//     };
-//     const newList = [...contactsList, newContact];
-//     await fsPromises.writeFile(contactsPath, JSON.stringify(newList), "utf-8");
-//     console.table(newList);
-//   } catch (err) {
-//     throw err;
-//   }
-// }
-// listContacts();
-
-// getContactById(2);
-
-// removeContact(11);
-
-// addContact("Diana", "diana@gmail.com", "098-768-65-77");
 
 module.exports = {
   listContacts: () => listContacts(),
-  getContactById: (id) => getContactById(id),
-  removeContact: (id) => removeContact(id),
+  getContactById: id => getContactById(id),
+  removeContact: id => removeContact(id),
   addContact: (name, email, phone) => addContact(name, email, phone),
 };
